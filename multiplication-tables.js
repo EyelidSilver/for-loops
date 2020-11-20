1cca360526787c080b119fe4f82c8f8612e28d0c
Aita K <67955300+EyelidSilver@users.noreply.github.com>
2020-11-20 15:30:49 +0100

    Add files via upload

---
+++ multiplication-tables.js
@@ -0,0 +1,19 @@
+for (i=2; i<=10; i++) {
+    let two = 2 * i;
+    let three = 3 * i;
+    let four = 4 * i;
+    let five = 5 * i;
+    let six = 6 * i;
+    let seven = 7 * i;
+    let eight = 8 * i;
+    let nine = 9 * i;
+    
+    console.log(i + ' * 2' + ' = ' + two);
+    console.log(i + ' * 3' +  ' = ' + three);
+    console.log(i + ' * 4' +  ' = ' + four);
+    console.log(i + ' * 5' +  ' = ' + five);
+    console.log(i + ' * 6' +  ' = ' + six);
+    console.log(i + ' * 7' +  ' = ' + seven);
+    console.log(i + ' * 8' + ' = ' + eight);
+    console.log(i + ' * 9' +  ' = ' + nine);
+}
